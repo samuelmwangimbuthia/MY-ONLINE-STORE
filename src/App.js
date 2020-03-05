706a2ba7c1378fe578a8280f9798026226c91bc4
samuel mbuthia <mbuthia.jsamuel@gmail.com>
2020-03-05 06:12:16 +0300

    Testing markup rendering

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          I've made some changes
         </p>
         <a
           className="App-link"
